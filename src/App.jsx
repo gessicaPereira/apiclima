5ac9cdbbb6bc896dcf1dcf1f1d5bc5908b406883
gessicaPereira <gessica.pfs@gmail.com>
2022-09-03 11:06:37 -0300

    estrturação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+
+//const url='https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=7083ac30a562875979b4b58788ded193'
+
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home(){
+    return(
+        <div className="Home">
+            <p>Localização</p>
+            <p>Descrição</p>
+            <p>Temperatura</p>
+            <p>Sensação</p>
+            <p>Umidade</p>
+            <p>Vento</p>
+        </div>
+    )
+}
